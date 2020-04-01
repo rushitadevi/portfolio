a2a07264c8c2a21e200beb8aed0766fbe080da20
Rushita <rushi.devi@gmail.com>
2020-04-01 15:45:02 +0200

    changes done

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -18,28 +18,23 @@ class Projects extends Component {
                                 <li>
                                     <div className="card">
                                         <div className="divImg">
-                                            <div style={{ position: "relative", paddingBottom: "56.25%", height: "0" }}>
-                                                <div style={{ position: "relative", paddingBottom: "56.25%", height: "0" }}>
-                                                    <iframe src="https://www.loom.com/embed/0aad25fc62c940f1b62c42f1a9314203" frameborder="0"
-                                                        webkitallowfullscreen mozallowfullscreen allowfullscreen
-                                                        style={{
-                                                            position: "absolute", top: "0", left: "0", width: "100%",
-                                                            height: "100%"
-                                                        }}
-                                                    ></iframe>
-                                                </div>
+                                            <div style={{position: "relative", paddingBottom: "56.25%", height: "0"}}>
+                                                <iframe src="https://www.loom.com/embed/686d24c218a54e1bab61e249be13f2a9" frameborder="0"
+                                                    webkitallowfullscreen mozallowfullscreen allowfullscreen
+                                                    style={{position: "absolute", top: "0", left: "0", width: "100%", height: "100%"}}>
+                                                </iframe>
                                             </div>
                                         </div>{/* end of divImg */}
                                         <Scrollbars style={{ height: 300 }}>
                                             <ul className="card__content">
-                                                <h3>Mini Travel Guide</h3>
+                                                <h3>Shopping Cart</h3>
                                                 <li className="list-group-item">HTML</li>
                                                 <li className="list-group-item">Bootstrap</li>
-                                                <li className="list-group-item">React</li>
+                                                <li className="list-group-item">Javascript</li>
                                             </ul> {/* end of card_content */}
                                         </Scrollbars>
                                         <div className="card-footer">
-                                            <a href="https://github.com/rushitadevi/mini-travel-guide" >Github Repo</a>
+                                            <a href="https://github.com/rushitadevi/ShopingCart" >Github Repo</a>
                                         </div>
                                     </div> {/* end of card */}
                                 </li> {/* end of li */}
@@ -50,10 +45,10 @@ class Projects extends Component {
                                 <li>
                                     <div className="card">
                                         <div className="divImg">
-                                            <div style={{position: "relative", paddingBottom: "56.25%", height: 0}}>
+                                            <div style={{ position: "relative", paddingBottom: "56.25%", height: 0 }}>
                                                 <iframe src="https://www.loom.com/embed/cf18502137814044a4cbd830332e448d" frameborder="0"
                                                     webkitallowfullscreen mozallowfullscreen allowfullscreen
-                                                    style={{position: "absolute" , top: 0, left: 0 ,  width: "100%" , height: "100%"}} >
+                                                    style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }} >
                                                 </iframe>
                                             </div>
                                         </div>
@@ -78,22 +73,18 @@ class Projects extends Component {
                                 <li>
                                     <div className="card">
                                         <div className="divImg">
-                                            <div style={{ position: "relative", paddingBottom: "56.25%", height: "0" }}>
-                                                <div style={{ position: "relative", paddingBottom: "56.25%", height: "0" }}>
-                                                    <iframe src="https://www.loom.com/embed/0aad25fc62c940f1b62c42f1a9314203" frameborder="0"
-                                                        webkitallowfullscreen mozallowfullscreen allowfullscreen
-                                                        style={{
-                                                            position: "absolute", top: "0", left: "0", width: "100%",
-                                                            height: "100%"
-                                                        }}
-                                                    ></iframe>
-                                                </div>
+                                            <div style={{ position="relative", paddingBottom: "56.25%", height: "0" }}>
+                                                <iframe src="https://www.loom.com/embed/8a71dd63f36f42169a2866bc950d90c7" frameborder="0"
+                                                    webkitallowfullscreen mozallowfullscreen allowfullscreen
+                                                    style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }}>
+                                                </iframe>
                                             </div>
                                         </div>
                                         <Scrollbars style={{ height: 300 }}>
                                             <ul className="card__content">
                                                 <h3>Tracker</h3>
                                                 <li className="list-group-item">HTML</li>
+                                                <li className="list-group-item">CSS3</li>
                                                 <li className="list-group-item">Bootstrap</li>
                                                 <li className="list-group-item">React</li>
                                                 <li className="list-group-item">Redux</li>
